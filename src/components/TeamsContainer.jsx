89eeafdd7874afbea3512483d7bb794a30c84392
writecody <36807056+writecody@users.noreply.github.com>
2024-08-19 18:46:12 -0500

    add background image to TeamsContainer with inline style prop

--- src/components/TeamsContainer.jsx
+++ src/components/TeamsContainer.jsx
@@ -1,19 +1,27 @@
 import { teamsInfo } from "../data/teamInfo";
+import backgroundImage from "../assets/cartoon-soccer-ball-on-field.png";
 
 export default function TeamsContainer() {
   return (
     <>
-      <div className="teams-container">
+      <div
+        className="teams-container"
+        style={{ 
+            background: `url(${backgroundImage}`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat" 
+        }}
+      >
         {teamsInfo.map((object, index) => {
           return (
             <>
               <div key={index} className="teams-container__logo-container">
-                
-                  <img
-                    className="teams-container__logo"
-                    src={object.logo_image}
-                    alt={object.alt_text}
-                  />
+                <img
+                  className="teams-container__logo"
+                  src={object.logo_image}
+                  alt={object.alt_text}
+                />
               </div>
             </>
           );
