81c22cc7ce89f0c70a64f945325658beb408cd02
writecody <36807056+writecody@users.noreply.github.com>
2024-08-19 18:03:02 -0500

    add Header component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header></Header>
+      <main>
+        LOGO BUTTONS
+        About Link
+      </main>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  const letters = ["B", "U", "N", "D", "E", "S", "L", "I", "G", "A"];
+
+  return (
+    <header className="header"> 
+      {letters.map((letter, index) => (
+        <h1 key={index} className="header__letter">
+          {letter}
+        </h1>
+      ))}
+    </header>
+  );
+}
