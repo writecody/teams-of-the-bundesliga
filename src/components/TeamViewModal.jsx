963852dfbf992d61e5f9ae9def308fb284eb5b80
writecody <36807056+writecody@users.noreply.github.com>
2024-08-23 20:03:43 -0500

    style TeamViewModal

--- src/components/TeamViewModal.jsx
+++ src/components/TeamViewModal.jsx
@@ -8,8 +8,8 @@ const TeamViewModal = forwardRef(function TeamViewModal({
   return (
     <dialog ref={ref} className="team-view">
       <h2>{selectedTeam.name}</h2>
-      <img src={selectedTeam.logo_image} alt="Logo image" /> 
-      <p className="team-view__caption">TEAM DESCRIPTION</p>
+      <img src={selectedTeam.logo_image} alt={selectedTeam.logo_image} /> 
+      <p className="team-view__caption">{selectedTeam.nickname}</p>
       <form method="dialog">
         <button onClick={onClose}>Close</button>
       </form>
