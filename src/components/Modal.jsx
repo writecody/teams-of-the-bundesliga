4f09cdd4059c1adb0f3db950eb6a8c65cae24a37
writecody <36807056+writecody@users.noreply.github.com>
2024-09-02 18:34:18 -0500

    use team color from data file to dynamically style Modal component content

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,12 @@
-export default function Modal({teamToView = null, handleClose }) {
+export default function Modal({ teamToView = null, handleClose }) {
   return (
-    <dialog className="team-view" open>
-      <h2 className="team-view__header">{teamToView.name}</h2>
+    <dialog className="team-view backdrop" open>
+      <h2
+        className="team-view__header"
+        style={{ color: `${teamToView.primary_color}` }}
+      >
+        {teamToView.name}
+      </h2>
       <img
         className="team-view__logo-image"
         src={teamToView.logo_image}
